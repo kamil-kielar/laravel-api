078825888836b789aa96e6559589d45351ac8d69
Kamil Kielar <me@kamilkielar.co.uk>
2026-01-27 13:57:02 +0100

    create seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
+use App\Models\Book;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,6 +22,19 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
         ]);
+
+        // Create test Authors and Books
+        $authors = Author::factory()->count(20)->create();
+
+        Book::factory()
+            ->count(50)
+            ->create()
+            ->each(function ($book) use ($authors) {
+                $book->authors()->attach(
+                    $authors->random(rand(1, 3))->pluck('id')->toArray()
+                );
+            });
     }
 }
