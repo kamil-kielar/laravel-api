74500fb3c7760af35a9b1de840b219271508b83b
Kamil Kielar <me@kamilkielar.co.uk>
2026-01-29 21:31:56 +0100

    add routing

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\AuthorController;
+use App\Http\Controllers\Api\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Authentication
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Public endpoints (GET only)
+Route::get('books', [BookController::class, 'index']);
+Route::get('books/{book}', [BookController::class, 'show']);
+Route::get('authors', [AuthorController::class, 'index']);
+Route::get('authors/{author}', [AuthorController::class, 'show']);
+
+// Protected endpoints (requires authentication)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('books', [BookController::class, 'store']);
+    Route::put('books/{book}', [BookController::class, 'update']);
+    Route::delete('books/{book}', [BookController::class, 'destroy']);
+});
