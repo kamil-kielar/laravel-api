f4e7566ccf3e1accdc04e1dffb6f17726831d930
Kamil Kielar <me@kamilkielar.co.uk>
2026-01-29 20:57:28 +0100

    add resources data tests

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'full_name' => $this->full_name,
+            'last_book_title' => $this->last_book_title,
+
+            'books' => BookResource::collection($this->whenLoaded('books')),
+            'books_count' => $this->when(
+                $this->relationLoaded('books'),
+                fn() => $this->books->count()
+            ),
+
+            'created_at' => $this->created_at?->toISOString(),
+            'updated_at' => $this->updated_at?->toISOString(),
+        ];
+    }
+}
